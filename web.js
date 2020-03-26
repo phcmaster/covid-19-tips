1ab511870f586e7716354d507c37fc25ebf41ddc
Pedro Henrique <phcmaster@hotmail.com>
2020-03-26 00:56:48 -0300

    deploy app to heroku index.html

--- web.js
+++ web.js
@@ -1,4 +1,4 @@
 var express = require("express");
 var app = express();
-app.use(express.static(__dirname + "/src/index.html")); //aqui você define onde está o index.html da sua aplicação.
+app.use(express.static(__dirname + "/dist/covid19Tips/index.html")); //aqui você define onde está o index.html da sua aplicação.
 app.listen(process.env.PORT || 3000);
