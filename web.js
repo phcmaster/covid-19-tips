13224c7ad36d2b23c8a5a5566667bdc0e796e5c1
Pedro Henrique <phcmaster@hotmail.com>
2020-03-26 01:08:05 -0300

    deploy app to heroku index.html path

--- web.js
+++ web.js
@@ -1,4 +1,21 @@
-var express = require("express");
-var app = express();
-app.use(express.static(__dirname + "/dist/covid19Tips/index.html")); //aqui você define onde está o index.html da sua aplicação.
-app.listen(process.env.PORT || 3000);
+// var express = require("express");
+// var app = express();
+// app.use(express.static(__dirname + "/dist/covid19Tips")); //aqui você define onde está o index.html da sua aplicação.
+// app.listen(process.env.PORT || 3000);
+
+//Importa as dependências que acabamos de instalar
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
+app.use(express.static(__dirname + '/dist/covid19Tips'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/covid19Tips/index.html'));
+});
+
+// Inicia a aplicação pela porta configurada
+app.listen(process.env.PORT || 8080);
